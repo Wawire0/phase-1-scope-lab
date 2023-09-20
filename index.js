6fbd56cf93af2838188d7c18b323c57ad8cfb5e3
sandra <sandra.wawire@student.moringaschool.com>
2023-09-21 01:52:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+var customerName;
+function upperCaseCustomerName () {
+    if(customerName) {
+        customerName=customerName.Uppercase ();
+    }
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer ='someone'
+
+function changeLeastFavoriteCustomer() {
+    // Attempting to change the constant will result in an error
+    leastFavoriteCustomer = 'new person'; // This will throw an error
+  }
+  
+  
+  
+  
+  
+  
+  
